5c28ff2ba86e59e18e15f14c4e5ba7cf7d202478
Matteo Sung <oc-87948e@agentmail.to>
2026-08-07 08:22:22 +0000

    lockvet 0.4.8

--- Formula/lockvet.rb
+++ Formula/lockvet.rb
@@ -6,26 +6,26 @@
 class Lockvet < Formula
   desc "Explain any lockfile change: bumps, vulns, release ages, deprecations"
   homepage "https://github.com/matteo-sung/lockvet"
-  version "0.4.7"
+  version "0.4.8"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/matteo-sung/lockvet/releases/download/v0.4.7/lockvet_v0.4.7_darwin_arm64.tar.gz"
-      sha256 "44ae21c8ea6c2d780e46150f2f5c5986785e066d535f81e7e98485296b40c44c"
+      url "https://github.com/matteo-sung/lockvet/releases/download/v0.4.8/lockvet_v0.4.8_darwin_arm64.tar.gz"
+      sha256 "c4c00d4f3da76bc03772c47952fa8a24c6e8afba54c0cc4e4964834023c23671"
     else
-      url "https://github.com/matteo-sung/lockvet/releases/download/v0.4.7/lockvet_v0.4.7_darwin_amd64.tar.gz"
-      sha256 "9eb98ffad22b7e1a9d001a5bfb69c3bba1f86bb04b1d4c7b2ab0da998666794f"
+      url "https://github.com/matteo-sung/lockvet/releases/download/v0.4.8/lockvet_v0.4.8_darwin_amd64.tar.gz"
+      sha256 "2ff0eea8d80267c4daf6755ae17b7e0c943c7a8965a05ee62f687587265c47bf"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/matteo-sung/lockvet/releases/download/v0.4.7/lockvet_v0.4.7_linux_arm64.tar.gz"
-      sha256 "d27f2c4eeee259a08b5398b252ddb182d59c8348884291537076703b11b125bf"
+      url "https://github.com/matteo-sung/lockvet/releases/download/v0.4.8/lockvet_v0.4.8_linux_arm64.tar.gz"
+      sha256 "679f109fda0b43a8aa6e741892195777693d4fdd0c38ef970c48f187a40fd649"
     else
-      url "https://github.com/matteo-sung/lockvet/releases/download/v0.4.7/lockvet_v0.4.7_linux_amd64.tar.gz"
-      sha256 "bfcd40f5933eda0f6c191961562db4fccc279cc0f13019b8dd81573f53e6e1df"
+      url "https://github.com/matteo-sung/lockvet/releases/download/v0.4.8/lockvet_v0.4.8_linux_amd64.tar.gz"
+      sha256 "8ca261fcb5286d5d230c52c49178f1d74d56a4c12a47aec4627427bcfc630d3d"
     end
   end
 
