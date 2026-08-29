09bd6d9c54607f78552734237328ceb38687238d
Matteo Sung <oc-87948e@agentmail.to>
2026-08-29 04:35:14 +0000

    lockvet 0.6.7

--- Formula/lockvet.rb
+++ Formula/lockvet.rb
@@ -6,26 +6,26 @@
 class Lockvet < Formula
   desc "Explain any lockfile change: bumps, vulns, release ages, deprecations"
   homepage "https://github.com/matteo-sung/lockvet"
-  version "0.6.6"
+  version "0.6.7"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/matteo-sung/lockvet/releases/download/v0.6.6/lockvet_v0.6.6_darwin_arm64.tar.gz"
-      sha256 "2dc70a4db16bb75fe878a7d045a453d9fd985fa2e7c36b0040736a94d18b4abb"
+      url "https://github.com/matteo-sung/lockvet/releases/download/v0.6.7/lockvet_v0.6.7_darwin_arm64.tar.gz"
+      sha256 "f8d09b708206c0e0b68e19c842e7a80b0a369bd8a55ac2b857d83ddc1036eca7"
     else
-      url "https://github.com/matteo-sung/lockvet/releases/download/v0.6.6/lockvet_v0.6.6_darwin_amd64.tar.gz"
-      sha256 "47379ec94f04cb77ebcced7196f9c9a68d4d4d75286f3108c95176172c1b557c"
+      url "https://github.com/matteo-sung/lockvet/releases/download/v0.6.7/lockvet_v0.6.7_darwin_amd64.tar.gz"
+      sha256 "6f8ec75453c9be268a7128da35ea6a30642fc787b9beb601929645ac96a36501"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/matteo-sung/lockvet/releases/download/v0.6.6/lockvet_v0.6.6_linux_arm64.tar.gz"
-      sha256 "f8e3748f83defa2842d0ada26a8acefb0c5d8668aca214291870abd4a41d28b3"
+      url "https://github.com/matteo-sung/lockvet/releases/download/v0.6.7/lockvet_v0.6.7_linux_arm64.tar.gz"
+      sha256 "57498b4c022730af5e024fd4ac6cf134032771a354a5cf9cfdaa9d05cff163a2"
     else
-      url "https://github.com/matteo-sung/lockvet/releases/download/v0.6.6/lockvet_v0.6.6_linux_amd64.tar.gz"
-      sha256 "0dfdd5069eefe723ecda6351176d2da7d34a0bb5e4fbe17a7163059575dc8c10"
+      url "https://github.com/matteo-sung/lockvet/releases/download/v0.6.7/lockvet_v0.6.7_linux_amd64.tar.gz"
+      sha256 "254da5e647acc1df3bcf5656b830e77cfaa1c10177a4608e9c78266136917f90"
     end
   end
 
